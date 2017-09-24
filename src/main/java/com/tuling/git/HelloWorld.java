fb04aea1a1f9367a3d7b61ef9707e5567670b132
shdosh <shdoshzdd@163.com>
2017-09-24 15:27:21 +0800

    Initial commit

---
+++ src/main/java/com/tuling/git/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.tuling.git;
+
+/**
+ * Created by shdoshzdd on 2017/9/24.
+ */
+public class HelloWorld {
+
+    public static void sayHello(){
+        System.out.println("Hello World");
+    }
+
+}
